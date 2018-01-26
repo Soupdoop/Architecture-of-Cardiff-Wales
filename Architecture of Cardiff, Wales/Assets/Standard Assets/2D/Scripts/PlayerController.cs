a4dbeb7eef6c6df07af024d01872220f6e10621c
Jack Herd <jherd23@gmail.com>
2018-01-26 18:18:02 -0500

    Blauhg

---
+++ Architecture of Cardiff, Wales/Assets/Standard Assets/2D/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float friction;
+
+	public float speed;
+
+	public float jump;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float newVerticalVelocity = 0;
+		if(Input.GetAxis("Vertical") > 0 && gameObject.GetComponent<Rigidbody2D>().velocity.y == 0) {
+			newVerticalVelocity = jump;
+		} else {
+			newVerticalVelocity = gameObject.GetComponent<Rigidbody2D>().velocity.y;
+		}
+		float newHorizontalVelocity = Input.GetAxis("Horizontal") * speed;
+		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(newHorizontalVelocity, newVerticalVelocity);
+		//gameObject.GetComponent<Rigidbody2D>().velocity *= (1 - friction);
+	}
+}
