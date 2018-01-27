11e6c0da129a00d8e4bde344438c9b48d0b249f4
Zachary Danz <zsd4yr@virginia.edu>
2018-01-27 15:24:34 -0500

    please do this

---
+++ Architecture of Cardiff, Wales/Assets/MusicMgmr.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class MusicMgmr : MonoBehaviour {
+
+	public AudioMixer master;
+
+	void Awake() {
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
