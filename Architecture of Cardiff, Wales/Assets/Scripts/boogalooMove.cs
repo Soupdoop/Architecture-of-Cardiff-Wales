2c0b299d2f1fd365f4db3296ce5e1738dba0c641
WyotJunctor <wjoyner127@gmail.com>
2018-01-27 18:51:58 -0500

    MainMenu dunzo?

---
+++ Architecture of Cardiff, Wales/Assets/Scripts/boogalooMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boogalooMove : MonoBehaviour {
+
+    private Vector3 target;
+    private float smoothSpeed = 5f;
+
+    private void Start() {
+        target = new Vector3(450, 400, 0);
+        Debug.Log(target);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        transform.position = Vector3.MoveTowards(transform.position, target, smoothSpeed);
+    }
+}
