4ee289d24b793bb1f86dfcd3bb135ec65be97676
rmccampbell <rmccampbell7@gmail.com>
2018-01-26 21:05:31 -0500

    actually added infection script

---
+++ Architecture of Cardiff, Wales/Assets/Scripts/Infection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Infection : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        var other = coll.gameObject.GetComponent<Activatable>();
+        if (other)
+        {
+            other.Activate();
+        }
+    }
+}
