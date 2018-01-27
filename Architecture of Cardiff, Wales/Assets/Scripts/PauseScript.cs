44bb21314b62144356f80a6998dfd54e39a67c25
WyotJunctor <wjoyner127@gmail.com>
2018-01-27 10:27:18 -0500

    PauseMenuTesting added -need da assets

---
+++ Architecture of Cardiff, Wales/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenu;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown("p") || Input.GetKeyDown(KeyCode.Escape)) {
+            if (GameIsPaused) {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+    }
+
+    void Pause() {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Resume() {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void LoadMenu() {
+        //SceneManager.LoadScene("MainMenu");
+        Resume();
+        Debug.Log("MAIN MENU");
+    }
+
+    public void QuitGame() {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
