841775711bf62ac02d20178f2fdea1dea31e879e
ferociouself <jhe3cd@virginia.edu>
2018-01-27 15:49:47 -0500

    Enemy time

---
+++ Architecture of Cardiff, Wales/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject followed;
+	public float size;
+
+	// Use this for initialization
+	void Start () {
+		if (followed == null) {
+			followed = gameObject;
+		}
+		gameObject.GetComponent<Camera>().orthographic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = followed.transform.position;
+		gameObject.GetComponent<Camera>().orthographicSize = size;
+	}
+
+	public void ChangeFollowed(GameObject go) {
+		followed = go;
+	}
+
+	public void ChangeSize(float sz) {
+		size = sz;
+	}
+}
---
+++ Architecture of Cardiff, Wales/Assets/Scripts/Enemies/ChaseTarget.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseTarget : MonoBehaviour {
+
+	public string targetTag = "Infected";
+
+	public Sprite chasingSprite;
+	public Sprite idleSprite;
+
+	public float chaseSpeed = 1.0f;
+	public float maxDist = 1.0f;
+
+	public Rigidbody2D rb;
+	public SpriteRenderer sprite;
+
+	// Use this for initialization
+	void Start () {
+		if (rb == null) rb = GetComponent<Rigidbody2D>();
+		if (sprite == null) sprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject[] targets = GameObject.FindGameObjectsWithTag(targetTag);
+		float minDistance = float.MaxValue;
+		GameObject minObj = null;
+		foreach (GameObject go in targets) {
+			float dist = Vector2.Distance(transform.position, go.transform.position);
+			Debug.Log("Looking at " + go.name + " at distance of " + dist);
+			if (dist > maxDist) continue;
+			Debug.Log("Within max distance");
+			if (dist < minDistance) {
+				minDistance = dist;
+				minObj = go;
+			}
+		}
+		if (minObj == null) {
+			sprite.sprite = idleSprite;
+			rb.AddForce(-chaseSpeed*rb.velocity);
+		} else {
+			sprite.sprite = chasingSprite;
+			rb.AddForce((minObj.transform.position - transform.position).normalized * chaseSpeed);
+			sprite.flipX = minObj.transform.position.x > transform.position.x;
+			transform.localRotation = Quaternion.FromToRotation(transform.position, minObj.transform.position);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.CompareTag("Infected")) {
+			Destroy(coll.gameObject);
+		}
+	}
+}
