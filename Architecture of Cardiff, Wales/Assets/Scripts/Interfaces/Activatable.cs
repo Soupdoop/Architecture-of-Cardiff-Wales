3e8f3337acecf2ee1a025571ffdf0cd74275271b
ferociouself <jhe3cd@virginia.edu>
2018-01-26 19:39:36 -0500

    Unassuming Beginnings

---
+++ Architecture of Cardiff, Wales/Assets/Scripts/Interfaces/Activatable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Activatable : MonoBehaviour {
+
+	protected bool activated = true;
+
+	bool Activate() {
+		activated = true;
+		return activated;
+	}
+
+	bool Deactivate() {
+		activated = false;
+		return activated;
+	}
+}
