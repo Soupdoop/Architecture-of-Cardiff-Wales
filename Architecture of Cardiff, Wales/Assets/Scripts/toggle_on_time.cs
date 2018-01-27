6de11e30ebad5a934cfa08a0fd3ec75416dc213b
Zachary Danz <zsd4yr@virginia.edu>
2018-01-27 13:19:21 -0500

    world 1-1

---
+++ Architecture of Cardiff, Wales/Assets/Scripts/toggle_on_time.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggle_on_time : MonoBehaviour {
+
+	public GameObject first;
+	public GameObject second;
+
+	public float time_to_switch;
+	public float random_mod;
+
+	private float timer;
+
+	// Use this for initialization
+	void Start () {
+		first.gameObject.SetActive (true);
+		second.gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer > time_to_switch) 
+		{
+			Swap ();
+		}
+	}
+
+	void Swap()
+	{
+		bool flop = first.gameObject.activeSelf;
+		first.gameObject.SetActive (!flop);
+		second.gameObject.SetActive (flop);
+		timer = 0.0f - Random.Range(0,random_mod);
+	}
+}
