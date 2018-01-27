e1bad263b68967701b508f692dfe6dcb7da70c6a
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2018-01-27 11:46:15 -0500

    car/spaceship movement

---
+++ Architecture of Cardiff, Wales/Assets/Scripts/Movement/CarMovement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : BasicMovement {
+
+	public float movementStrength = 1.0f;
+	public float topSpeed;
+	public float steerSpeed;
+	public float decelStrength;
+	private float currentSpeed;
+	private float tolerance = 0.05f;
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		#if DEBUG 
+		Debug.Log("This is a car starting.");
+		#endif
+		currentSpeed = 0;
+		if (rb == null) {
+			rb = GetComponent<Rigidbody2D>();
+		}
+	}
+
+	override protected void UpdateFields() {
+		rb.velocity = (Vector2)transform.right.normalized * currentSpeed; //always move toward heading
+	}
+
+	override protected void DoUpAction() {
+		#if DEBUG 
+		Debug.Log("Car Up!");
+		#endif
+
+		if (rb.velocity.magnitude < topSpeed) {
+			currentSpeed += movementStrength * Time.deltaTime;
+		}
+	}
+
+	override protected void DoDownAction() {
+		#if DEBUG 
+		Debug.Log("Car Down!");
+		#endif
+		if (rb.velocity.magnitude < topSpeed + tolerance) {
+			currentSpeed -= movementStrength * Time.deltaTime;
+		}
+	}
+
+	override protected void DoLeftAction() {
+		#if DEBUG 
+		Debug.Log("Car Left!");
+		#endif
+
+		rb.AddTorque (steerSpeed);
+	}
+
+	override protected void DoRightAction() {
+		#if DEBUG 
+		Debug.Log("Car Right!");
+		#endif
+
+		rb.AddTorque (-steerSpeed);
+	}
+
+	override protected void DoSpecialAction() {
+		#if DEBUG 
+		Debug.Log("Car Special!");
+		#endif
+
+		//		rb.AddForce(rb.velocity * movementStrength);
+	}
+
+	override protected void DoNeutralAction() {
+		rb.AddForce(decelStrength*-rb.velocity);
+	}
+}
--- Architecture of Cardiff, Wales/Assets/Scripts/Movement/SpaceshipMovement.cs
+++ Architecture of Cardiff, Wales/Assets/Scripts/Movement/SpaceshipMovement.cs
@@ -1,18 +1,17 @@
-﻿#define DEBUG
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GermMovement : BasicMovement {
+public class SpaceshipMovement : BasicMovement {
 
 	public float movementStrength = 1.0f;
+	public float steerSpeed;
 	public Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
 		#if DEBUG 
-		Debug.Log("This is a germ starting.");
+		Debug.Log("This is a spaceship starting.");
 		#endif
 		if (rb == null) {
 			rb = GetComponent<Rigidbody2D>();
@@ -24,46 +23,45 @@ public class GermMovement : BasicMovement {
 
 	override protected void DoUpAction() {
 		#if DEBUG 
-		Debug.Log("Germ Up!");
+		Debug.Log("Spaceship Up!");
 		#endif
 
-		rb.AddForce(Vector2.up * movementStrength);
+		rb.AddForce((Vector2)transform.right * movementStrength);
 	}
 
 	override protected void DoDownAction() {
 		#if DEBUG 
-		Debug.Log("Germ Down!");
+		Debug.Log("Spaceship Down!");
 		#endif
 
-		rb.AddForce(Vector2.down * movementStrength);
+		rb.AddForce ((Vector2)transform.right * -movementStrength);
 	}
 
 	override protected void DoLeftAction() {
 		#if DEBUG 
-		Debug.Log("Germ Left!");
+		Debug.Log("Spaceship Left!");
 		#endif
 
-		rb.AddForce(Vector2.left * movementStrength);
+		rb.AddTorque (steerSpeed);
 	}
 
 	override protected void DoRightAction() {
 		#if DEBUG 
-		Debug.Log("Germ Right!");
+		Debug.Log("Spaceship Right!");
 		#endif
 
-		rb.AddForce(Vector2.right * movementStrength);
+		rb.AddTorque (-steerSpeed);
 	}
 
 	override protected void DoSpecialAction() {
 		#if DEBUG 
-		Debug.Log("Germ Special!");
+		Debug.Log("Spaceship Special!");
 		#endif
 
-		rb.AddForce(rb.velocity * movementStrength);
+//		rb.AddForce(rb.velocity * movementStrength);
 	}
 
 	override protected void DoNeutralAction() {
-		rb.AddForce(-1*rb.velocity);
+//		rb.AddForce(-1*rb.velocity); //no deceleration for spaceships
 	}
-
 }
