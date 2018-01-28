348fbefae1746f2adeadcbeb088e6016b6bbbe89
rmccampbell <rmccampbell7@gmail.com>
2018-01-28 02:42:58 -0500

    started galaxy levels

---
+++ Architecture of Cardiff, Wales/Assets/Scripts/GalaxyControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GalaxyControl : Activatable {
+
+    public Activatable armadaPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override bool Activate()
+    {
+        base.Activate();
+        Activatable armada = Instantiate(armadaPrefab, transform.position, Quaternion.identity);
+        armada.Activate();
+        return activated;
+    }
+
+}
--- Architecture of Cardiff, Wales/Assets/Scripts/Infection.cs
+++ Architecture of Cardiff, Wales/Assets/Scripts/Infection.cs
@@ -43,6 +43,7 @@ public class Infection : MonoBehaviour
         }
 
 	}
+
 	void OnCollisionEnter2D(Collision2D coll)
 	{
 		if (gameObject.GetComponent<Activatable> ().hasBeenActivated) {
@@ -56,4 +57,21 @@ public class Infection : MonoBehaviour
 			}
 		}
 	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (gameObject.GetComponent<Activatable>().hasBeenActivated)
+        {
+            var other = coll.gameObject.GetComponent<Activatable>();
+            if (other && !other.hasBeenActivated)
+            {
+                other.Activate();
+                var otherinfection = coll.gameObject.GetComponent<Infection>();
+                if (otherinfection)
+                {
+                    otherinfection.timeUntilDeath = otherinfection.lifetime;
+                }
+            }
+        }
+    }
 }
